c7ab8232b41f7fdbf0fece04edc760380314a831
philmill <phil@philmill.net>
2016-03-02 10:33:15 -0500

    feat: reqwest example with google books

---
+++ src/entry.js
@@ -0,0 +1,118 @@
+import React, {
+  Component,
+  PropTypes
+} from 'react';
+import ReactDOM from 'react-dom';
+window.React = React;
+
+import marked from 'marked';
+import reqwest from 'reqwest';
+
+class CommentBox extends Component {
+  static propTypes = {
+    url: PropTypes.string.isRequired,
+    data: PropTypes.array
+  }
+
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    reqwest({
+      url: this.props.url,
+      method: 'get',
+      success: (resp) => {
+        console.log(resp);
+        this.setState({data: this.reduceToList(resp)});
+      },
+      error: (err) => { console.log(err.toString()); }
+    });
+  }
+
+  reduceToList(resp) {
+    return (
+      resp.items.map((item, index) => {
+        return ({
+          id: index,
+          author: item.volumeInfo.authors.toString(),
+          text: (item.volumeInfo.description === undefined) ?
+            'no description' :
+            item.volumeInfo.description
+        });
+      })
+    );
+  }
+
+  render() {
+    return (
+      <div className="commentBox">
+        <CommentList comments={this.state.data}/>
+        <CommentForm />
+      </div>
+    );
+  }
+}
+
+class CommentList extends Component {
+  static propTypes = {
+    comments: PropTypes.array.isRequired
+  }
+
+  render() {
+    const commentNodes = this.props.comments.map((comment) => {
+      return (
+        <Comment
+          key={comment.id}
+          author={comment.author}
+        >
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+}
+
+class CommentForm extends Component {
+  render() {
+    return (
+      <div className="commentForm">
+        Hello I'm a CommentForm
+      </div>
+    );
+  }
+}
+
+class Comment extends Component {
+  static propTypes = {
+    author: PropTypes.string.isRequired,
+    children: PropTypes.any
+  }
+
+  rawMarkup = () => {
+    const markup = marked(this.props.children.toString(), {sanitize: true});
+    return { __html: markup};
+  }
+
+  render() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <CommentBox url="https://www.googleapis.com/books/v1/volumes?q=cat+inauthor:kurt+vonnegut"/>,
+  document.getElementById('content')
+);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: {
+    app: ['./src/entry.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: [
+          path.resolve(__dirname, 'node_modules')
+        ],
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015'],
+          plugins: [
+            'transform-object-rest-spread',
+            'transform-class-properties'
+          ]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ]
+};
