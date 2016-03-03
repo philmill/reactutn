a2b22e698bc5f224096c7090b64384ad80b245a1
philmill <phil@philmill.net>
2016-03-02 21:35:01 -0500

    feat: create build and update public path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    publicPath: '/assets/',
+    publicPath: '/build/',
     filename: 'bundle.js'
   },
   module: {
